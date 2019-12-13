0a2356ecf0776e6085cebe9071e0bf68e670907e
turbk <37018038+turbk@users.noreply.github.com>
2019-12-13 22:19:06 +0100

    Add files via upload
    https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
    PRACTICE PYTHON
    Exercise 1

---
+++ practice1.py
@@ -0,0 +1,6 @@
+print("Hi, give me ur name and age")
+x = input("Your name ")
+y = int(input("Your age "))
+s = 100 - y
+rok = 2019 + s
+print("hello", x, "you are", y, "old, and will turn 100 in the year: ", rok)
