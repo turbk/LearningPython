afdedfb898e43c148b2ed4ae89bc6a621d15555f
turbk <37018038+turbk@users.noreply.github.com>
2019-12-19 20:56:40 +0100

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,7 @@
+print("Please enter a number in order to determine wheter said number is even or odd")
+num1 = int(input())
+print(num1, "Checking...")
+if num1 % 2:
+    print("It's odd")
+else:
+    print("It's even ")
